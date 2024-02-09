9fa47f8ef0097928ebf29469fa03324851b00a18
Jasur <jasurfozilov42@gmail.com>
2024-02-09 18:45:54 +0500

    3 commit

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -55,7 +55,6 @@ class ValidatorTests {
 		ConstraintViolation<Person> violation = constraintViolations.iterator().next();
 		assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
 		assertThat(violation.getMessage()).isEqualTo("must not be blank");
-//		assertThat(violation.getMessage()).isEqualTo("must be empty");
 	}
 
 }
